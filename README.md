7c68e6bed7542a2fb24ca06b846986e29c9230af
Paul Hadfield <hadfieldp@google.com>
2017-10-18 11:10:10 +0100

    Skeleton for natas15 puzzle

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# natas
+Here are some programs I wrote to help crack the natas web exploits challenges here:
+http://overthewire.org/wargames/natas/
---
+++ natas15/crack.go
@@ -0,0 +1,10 @@
+// crack is a little program to discover a base64-encoded password using an oracle in the natas15 challenge page
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, playground")
+}
